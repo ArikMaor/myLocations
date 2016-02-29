b4b6ca547e8b6e80de20ae8b77f11714ab63e2a1
arikmaor <arikmaor@gmail.com>
2016-02-29 17:43:06 +0200

    filtering by categories

--- src/locations/locations.controller.js
+++ src/locations/locations.controller.js
@@ -1,3 +1,15 @@
 export default class LocationsController {
+  constructor($scope) {
+    'ngInject';
 
+    Object.assign(this, {
+      _categories: $scope.categories
+    })
+  }
+
+  isVisible(location) {
+    return location.categories.find(categoryId =>
+      this._categories.find(category =>
+        category.id == categoryId && category.selected));
+  }
 }
