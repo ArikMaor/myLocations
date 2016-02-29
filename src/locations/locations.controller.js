57edbbf6aee566019cb9c471acccc83d94a51e3d
arikmaor <arikmaor@gmail.com>
2016-02-29 16:08:58 +0200

    adding buttons with fontawesome

--- src/locations/locations.controller.js
+++ src/locations/locations.controller.js
@@ -7,7 +7,7 @@ export default class LocationsController {
       name: 'Home',
       address: 'Rama 26',
       coordinates: [ 1, 2 ],
-      categories: [ 1, 3]
+      categories: [ 1, 3 ]
     }];
 
     let storage = $localStorage.$default({
