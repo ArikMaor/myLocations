f43b62d3e1a27f0ad7b70a04732a087f28cadc59
arikmaor <arikmaor@gmail.com>
2016-03-04 00:30:52 +0200

    adding empty location state

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,8 @@ import pageNotFoundTemplate from './assets/404.html'
 import './views/main.scss'
 import mainTemplate from './views/main.html'
 import mainController from './views/main.controller.js'
+import locationTemplate from './views/location.html'
+import locationController from './views/location.controller.js'
 
 export default function($stateProvider, $urlRouterProvider) {
   'ngInject';
@@ -14,10 +16,11 @@ export default function($stateProvider, $urlRouterProvider) {
       controller: mainController,
       controllerAs: 'main'
     })
-    // .state('categories', {
-    //   url: "/categories",
-    //   template: categoriesTemplate
-    // })
+    .state('location', {
+      url: "/location/:name",
+      template: locationTemplate,
+      controller: locationController
+    })
     .state('404', {
       url: "/404",
       template: pageNotFoundTemplate
