7fafcfda9eba43d69245809c1cf0a2a4998da23d
arikmaor <arikmaor@gmail.com>
2016-03-03 23:43:29 +0200

    removing obsolete code

--- src/categories/categories.module.js
+++ src/categories/categories.module.js
@@ -3,10 +3,8 @@ import 'ngstorage'
 
 import CategoryService from './category.service'
 import CategorySelector from './category-selector.directive'
-import CategoryListEditor from './category-list-editor.directive'
 
 export default angular.module('myLocations.categories', [ 'ngStorage' ])
   .service("categoryService", CategoryService)
   .directive('categorySelector', CategorySelector)
-  .directive('categoryListEditor', CategoryListEditor)
   .name;
